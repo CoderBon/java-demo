901624bb0b471e0d383886114c4ef4edf5ba79f2
luo <Luo@qq.com>
2021-12-29 14:41:38 +0800

    first commit

---
+++ java-demo/src/main/java/com/xiaoluo/demo/controller/CounterController.java
@@ -0,0 +1,20 @@
+package com.xiaoluo.demo.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class CounterController {
+
+    @Autowired
+    StringRedisTemplate redisTemplate;
+
+    @GetMapping("/hello")
+    public String count(){
+
+        long increment = redisTemplate.opsForValue().increment("count-people");
+        return "有【"+ increment +"】人访问了这页面";
+    }
+}
